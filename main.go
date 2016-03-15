57d62ab7c2ccb06bc9cbbca146002720adcf042f
Adrian Lanzafame <adrianlanzafame92@gmail.com>
2016-03-15 13:43:28 +1000

    Added extra handler

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ import (
 )
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
+	fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
+}
+
+func extraHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Hack With Me</h1>\n")
 }
 
 func main() {
-    http.HandleFunc("/", defaultHandler)
-    fmt.Println("Example app listening at http://localhost:8888")
-    http.ListenAndServe(":8888", nil)
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/hwm", extraHandler)
+	fmt.Println("Example app listening at http://localhost:8888")
+	http.ListenAndServe(":8888", nil)
 }
